153a3258cd0156b027abae7d04936f59d729a7f7
Lorrapaz <lorrannyyasmin6@gmail.com>
2025-07-10 22:46:58 -0300

    home made

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'media', // ou 'class' se preferir ativar com uma classe
+  theme: {
+    extend: {
+      colors: {
+        // Estilos adicionais para dark mode, se necessário
+      }
+    }
+  },
+};
